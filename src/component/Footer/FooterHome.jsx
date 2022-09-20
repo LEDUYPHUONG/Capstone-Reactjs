ccfd12c3c9fe03f72552ef670aabfb6cfcdb65ec
unknown <boyfriend7777777@gmail.com>
2022-09-21 03:50:29 +0700

    fix variableSCSS

--- src/component/Footer/FooterHome.jsx
+++ src/component/Footer/FooterHome.jsx
@@ -2,6 +2,44 @@ import React from 'react'
 
 export default function FooterHome() {
   return (
-    <div>FooterHome</div>
+    
+    <div className='footer'>
+      <div className='footer-top'>
+          <div className="container">
+            <div className="row container">
+              <div className="col-4 footer-gethelp">
+                <div className="footer-title">GET HELP</div>
+                <ul>
+                  <li><a href="#">Home</a></li>   
+                  <li><a href="#">Nike</a></li>
+                  <li><a href="#">Adidas</a></li>
+                  <li><a href="#">Contact</a></li>
+                </ul>
+              </div>
+              <div className="col-4 footer-support">
+                <div className="footer-title">SUPPORT</div>
+                <ul>
+                  <li><a href="#">About</a></li>   
+                  <li><a href="#">Contact</a></li>
+                  <li><a href="#">Help</a></li>
+                  <li><a href="#">Phone</a></li>
+                </ul>
+              </div>
+              <div className="col-4 footer-register">
+                <div className="footer-title">REGISTER</div>
+                <ul>
+                  <li><a href="#">register</a></li> 
+                  <li><a href="#">login</a></li>
+                  <li></li>
+                  <li></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+      </div>
+      <div className="footer-bottom">
+        <p className='creator'>© 2022 <a href='https://cybersoft.edu.vn'>Cybersoft</a> All Rights Reserved | Design Theme by <a href='https://cybersoft.edu.vn'>Trương Tấn Khải</a></p>
+      </div>
+    </div>
   )
 }
--- src/pages/Index/Index.jsx
+++ src/pages/Index/Index.jsx
@@ -1,7 +1,99 @@
-import React from 'react'
+import React from "react";
 
 export default function index() {
+ 
+
   return (
-    <div>index</div>
-  )
+    <div className="index">
+      <div className="container">
+        <div
+          id="carouselExampleDark"
+          className="carousel carousel-dark slide"
+          data-bs-ride="carousel"
+        >
+          <div className="carousel-indicators">
+            <button
+              type="button"
+              data-bs-target="#carouselExampleDark"
+              data-bs-slide-to={0}
+              className="active"
+              aria-current="true"
+              aria-label="Slide 1"
+            />
+            <button
+              type="button"
+              data-bs-target="#carouselExampleDark"
+              data-bs-slide-to={1}
+              aria-label="Slide 2"
+            />
+            <button
+              type="button"
+              data-bs-target="#carouselExampleDark"
+              data-bs-slide-to={2}
+              aria-label="Slide 3"
+            />
+          </div>
+          <div className="carousel-inner">
+            <div className="carousel-item active" data-bs-interval={10000}>
+              <img src="./img/Giay.png" className="d-block w-100" alt="..." />
+              <div className="carousel-caption d-none d-md-block">
+                <h5>Product name</h5>
+                <p>
+                  Product description ....
+                </p>
+                <div className="button-carousel">
+                  <button className="btn btn-primary">Buy now</button>
+                </div>
+              </div>
+            </div>
+            <div className="carousel-item" data-bs-interval={2000}>
+              <img src="./img/Giay.png" className="d-block w-100" alt="..." />
+              <div className="carousel-caption d-none d-md-block">
+                <h5>Product name</h5>
+                <p>
+                  Product description ....
+                </p>
+                <div className="button-carousel">
+                  <button className="btn btn-primary">Buy now</button>
+                </div>
+              </div>
+            </div>
+            <div className="carousel-item">
+              <img src="./img/Giay.png" className="d-block w-100" alt="..." />
+              <div className="carousel-caption d-none d-md-block">
+                <h5>Product name</h5>
+                <p>
+                  Product description ....
+                </p>
+                <div className="button-carousel">
+                  <button className="btn btn-primary">Buy now</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <button
+            className="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExampleDark"
+            data-bs-slide="prev"
+          >
+            <span className="carousel-control-prev-icon" aria-hidden="true" />
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button
+            className="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExampleDark"
+            data-bs-slide="next"
+          >
+            <span className="carousel-control-next-icon" aria-hidden="true" />
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
+      </div>
+
+      <h3 className="index-ProductFeature-title">Product Feature</h3>
+      <div className="index-renderProduct"></div>
+    </div>
+  );
 }
